7ebf5b0dba96b541f66e115bd04e39b0315879fb
sra124 <sra124@sfu.ca>
2023-03-16 17:36:56 -0700

    on my way to implement PCB

--- PCB.h
+++ PCB.h
@@ -12,8 +12,20 @@ struct PCB
     List* incomingMessages;
 };
 
-//create a process
-PCB* createProcess(enum Priority priority)
-{
-    
-}
+//C
+//create a process Pass priority from a main file and pass static pid to the process 
+//(it will first increment the value and after assign it to the process)
+//the last argument will be the appropriate ready queue (high, medium, low)
+
+PCB* createProcess(enum Priority, int&, Queue**);
+
+//F
+//Copy the currently running process and put it on the ready Q corresponding to the original process' priority.
+//Attempting to Fork the "init" process should fail. 
+//Pass the process to the fork function, pass the integer of current PID, pass the readyQueue corresponding to it;
+PCB* forkProcess(PCB*,int&, Queue**);
+
+//K
+//kill the named process and remove it from the system.
+//pass the process, 
+void killProcess(int, Queue**);
