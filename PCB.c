13e778a321fa731a03762499da887b63ee5bc5cc
Sviatoslav <94946672+MaterialI@users.noreply.github.com>
2023-03-21 17:15:23 -0700

    Revert the commit
    reverted

--- PCB.c
+++ PCB.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Queue.h"
+#include "list.h"
 #include "PCB.h"
 
 //C
@@ -8,18 +9,16 @@
 //what to pass: Pass priority from a main file and pass static pid to the process 
 //(it will first increment the value and after assign it to the process)
 //the last argument will be the appropriate ready queue (high, medium, low)
-typedef bool (*COMPARATOR_FN)(void* pItem, void* pComparisonArg);
-static searchforID(Queue** Q , COMPARATOR_FN pComparator, void* pComparisonArg ){};
 
 Process* createProcess(short pr, unsigned int* pid, Queue** Qs)
-{   List* test = Queue_create();
+{
     Process* aNew;
     if(!(aNew = (Process*)malloc(sizeof(Process))))
     {
         return 0;
     }
     //to insert in the appropriate list
-   // List_preprend(Qs[pr]->qList, aNew);
+    List_preprend(Qs[pr]->qList, aNew);
     aNew->processPriority = pr;
     if(!(aNew->incomingMessages = (Queue*)malloc(sizeof(Queue))))
     {
@@ -47,7 +46,7 @@ Process* forkProcess(Process* current, unsigned int* pid, Queue** Qs)
     {
         return 0;
     }
-    //List_preprend(Qs[current->processPriority]->qList, aNew);
+    List_preprend(Qs[current->processPriority]->qList, aNew);
     aNew->PID = (*pid)++;
     aNew->processPriority = current->processPriority;
     aNew->processState = Ready;
@@ -60,8 +59,35 @@ Process* forkProcess(Process* current, unsigned int* pid, Queue** Qs)
 //pass the process PID, and Queueueueueues
 //in case of success, returns 0, failure returns -1
 
-int killProcess(unsigned int pid, Queue** Qs)
+int killProcess(Process* currentRunning, unsigned int pid, Queue** Qs, Queue** srQs)
 {
+    if(currentRunning->PID == pid)
+    {
+        currentRunning->processState = Blocked;
+        free(currentRunning);
+        currentRunning = Dequeue(Qs);  //dequeue has to return the ptr to returned obj
+        currentRunning ->processState = Running;
+        
+    }
+    Node* current = searchReadyQueue(Qs, pid, 3);
+    if(current != 0){
+        List_remove(Qs);
+        free(current);
+    }   
+    else
+    {
+        current = searchReadyQueue(srQs, pid, 2);
+        if(current !=0)
+        {
+            List_remove(srQs);
+            free(current);    
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    return 0;
     
 }
 
@@ -72,7 +98,20 @@ int killProcess(unsigned int pid, Queue** Qs)
 //what to pass: just pass the pointer and the array of queues
 //in case of success, returns 0, failure returns -1
 
-int exitProcess(Process*, Queue**);
+int exitProcess(Process* current, Queue** Qs)
+{
+    if(current != 0)
+    {    
+        current->processState = Blocked;
+        free(current);
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+    //do we put another process from waiting queue to the running state?.
+}
 
 
 //Q
@@ -81,12 +120,7 @@ int exitProcess(Process*, Queue**);
 //what to pass: Process pointer and queues pointers
 //in case of success, returns 0, failure returns -1
 
-int quantumProcess(Process* running , Queue** Ready_Queues){
-    running->processState = Ready;
-    Enqueue(Ready_Queues[running->processPriority],running);
-    // To Do : 
-    // Think of Faliure cases 
-}
+int quantumProcess(Process*, Queue**);
 
 
 //S
@@ -96,7 +130,7 @@ int quantumProcess(Process* running , Queue** Ready_Queues){
 //char *msg (nullterminated message string, 40 char max); 
 //in case of success, returns 0, failure returns -1
 
-int sendProcess(Process*, unsigned int sPID, char* msg , Queue*);
+int sendProcess(Process*, unsigned int sPID, char* msg);
 
 
 //R
